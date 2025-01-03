1cc8c99d12722937f55f3b90384cc6a9063ab1bf
ict std <leaningteexpc@gmail.com>
2025-01-03 17:09:47 +0900

    resistUserProfile

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,19 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+// モデルクラスを継承
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
+use Illuminate\Database\Eloquent\Model;
+// Userモデルクラスを作成
+class User extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'userName',
+        'userNickname',
+        'gender',
+        'age',
+        'valid',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
--- resources/views/createuser.blade.php
+++ resources/views/createuser.blade.php
@@ -3,12 +3,28 @@
 /**
  * ユーザの新規登録ページ
  * helloページに設置されている新規登録ボタンをクリックすることで遷移できる
+ * ユーザ情報を登録する
  */
 ?>
 <!doctype html>
 <html>
   <body>
-    <h3>Create User Page</h3>
+    <h3>ユーザ新規登録</h3>
+    <form action="/createuser" method="POST">
+      @csrf
+      <label for="userName">ユーザ名:</label>
+      <input type="text" id="userName" name="userName" required><br>
+      <label for="userNickname">ニックネーム:</label>
+      <input type="text" id="userNickname" name="userNickname" required><br>
+      <label for="gender">性別:</label>
+      <input type="radio" id="male" name="gender" value="男" required>
+      <label for="male">男</label>
+      <input type="radio" id="female" name="gender" value="女" required>
+      <label for="female">女</label><br>
+      <label for="age">年齢:</label>
+      <input type="number" id="age" name="age" required><br>
+      <button type="submit">登録</button>
+    </form>
     <button onclick="window.location.href='/hello'">戻る</button>
   </body>
 </html>
